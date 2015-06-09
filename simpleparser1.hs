0af29489a81ea23bae3e11d170cf21b8c03e0e25
Brian Johnson <brian@brianjohnson.co.za>
2015-06-09 21:47:32 +0200

    Updated parser to ignore spaces.

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -3,14 +3,17 @@ import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO()
-main = do args <- getArgs
+main = do 
+  args <- getArgs
   putStrLn (readExpr (args !! 0))
 
-symbol :: Parser char
+symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
+spaces :: Parser ()
+spaces = skipMany1 space
